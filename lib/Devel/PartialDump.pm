f195147bb08446790c3681f8f75f47c09080c907
Karen Etheridge <ether@cpan.org>
2014-09-27 16:36:56 -0700

    add some keywords to metadata

--- lib/Devel/PartialDump.pm
+++ lib/Devel/PartialDump.pm
@@ -2,6 +2,7 @@ use strict;
 use warnings;
 package Devel::PartialDump;
 # ABSTRACT: Partial dumping of data structures, optimized for argument printing.
+# KEYWORDS: development debugging dump dumper diagnostics deep data structures
 # vim: set ts=8 sw=4 tw=78 et :
 
 use Carp ();
