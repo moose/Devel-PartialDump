9ea9e23b1e50f6940650e2b0a5f257101925e452
Karen Etheridge <ether@cpan.org>
2015-03-14 22:29:14 -0700

    fix vim modeline

--- lib/Devel/PartialDump.pm
+++ lib/Devel/PartialDump.pm
@@ -3,7 +3,7 @@ use warnings;
 package Devel::PartialDump;
 # ABSTRACT: Partial dumping of data structures, optimized for argument printing.
 # KEYWORDS: development debugging dump dumper diagnostics deep data structures
-# vim: set ts=8 sw=4 tw=78 et :
+# vim: set ts=8 sts=4 sw=4 tw=78 et :
 
 use Carp ();
 use Scalar::Util qw(looks_like_number reftype blessed);
